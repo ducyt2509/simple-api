f1ea677030c04a4f4295be09b74072c20e2dbc82
ductv-its <ductv@iceteasoftware.com>
2024-11-07 10:52:51 +0700

    Update deployment workflow and add PM2 ecosystem configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  apps: [
+    {
+      name: 'api-8001',
+      script: './app.js',
+      args: 'port 8001',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8001,
+      },
+    },
+    {
+      name: 'api-8002',
+      script: './app.js',
+      args: 'port 8002',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8002,
+      },
+    },
+    {
+      name: 'api-8003',
+      script: './app.js',
+      args: 'port 8003',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '200M',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 8003,
+      },
+    },
+  ],
+};
